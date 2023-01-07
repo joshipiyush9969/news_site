967e90b9e567c6b9d28bdab5e54a841c9bca1905
Piyush Joshi <piyushjb7@hotmail.com>
2023-01-08 02:12:59 +0530

    search bar added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,15 @@ import { Col, Row } from "react-bootstrap";
 import { BlogCard } from "../components/BlogCard";
 import { useNavContext } from "../context/NavBarContext";
 import data from "../data/items.json";
-import "./Home.css"
+import "./Home.css";
 
 export function Home() {
   const navFunctions = useNavContext();
   const currentNavState = navFunctions.NavState;
+  const [query, setQuery] = useState<string>("");
+
   return (
-    <div>
+    <div className="pt-5">
       {currentNavState() ? (
         <div className="buttonNav" onClick={navFunctions.openNav} tabIndex={0}>
           <span className="buttonText">Menu</span>
@@ -19,15 +21,25 @@ export function Home() {
           <span className="buttonText">Menu</span>
         </div>
       )}
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setQuery(e.target.value.toLowerCase())
+        }
+      />
       <Row md={2} xs={1} lg={3}>
-        {data.map((item) => (
-          <Col
-            key={item.id}
-            className="d-flex align-items-center justify-content-center"
-          >
-            <BlogCard {...item} isOpen={currentNavState()} />
-          </Col>
-        ))}
+        {data
+          .filter((news) => news.title.toLowerCase().includes(query))
+          .map((item) => (
+            <Col
+              key={item.id}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <BlogCard {...item} isOpen={currentNavState()} />
+            </Col>
+          ))}
       </Row>
       <div className="home_footer"></div>
     </div>
